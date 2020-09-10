cad14c71b6069abc9fcd8764abd59304d5378982
Andrei <stigmaby@gmail.com>
2020-09-10 23:10:45 +0300

    15 is done, Select component

--- src/Select/Select.stories.tsx
+++ src/Select/Select.stories.tsx
@@ -1,1 +1,36 @@
+import React, {useState} from 'react';
+import {Select, SelectPropsType} from "./Select";
+import {action} from "@storybook/addon-actions";
 
+export default {
+    title: 'Select',
+    component: Select,
+}
+
+export const WithValue: React.FC<SelectPropsType> = () => {
+    const [value, setValue] = useState('2')
+    return <>
+        <Select value={value}
+                onChange={setValue}
+                items={[
+                    {title: 'Grodno', value: '1'},
+                    {title: 'Minsk', value: '2'},
+                    {title: 'Brest', value: '3'}
+                ]}
+        />
+    </>
+}
+
+export const WithoutValue: React.FC<SelectPropsType> = () => {
+    const [value, setValue] = useState(null)
+    return <>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {title: 'Grodno', value: '1'},
+                    {title: 'Minsk', value: '2'},
+                    {title: 'Brest', value: '3'}
+                ]}
+        />
+    </>
+}
